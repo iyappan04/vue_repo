7ec536a7ff85559471161e51b5407054cfffd0a8
IYAPPAN <82354462+iyappan04@users.noreply.github.com>
2023-10-25 10:33:23 -0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+  <Tabs/>
+  <Acc/>
+  <Navbar :textColor="'blue'" />
+    <Navbar :textColor="'white'" />
+  </div>
+</template>
+
+<script>
+
+import Tabs from "./components/Tabs.vue"
+import Acc from "./components/Acc.vue"
+import Navbar from "./components/Navbar.vue"
+
+export default {
+  name: 'App',
+  components: {
+    Tabs,
+    Acc,
+    
+    Navbar
+  }
+}
+</script>
+
+<style>
+
+</style>
